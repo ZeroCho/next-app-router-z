4fcb4f1bb6035f0b7d8c23dda6f6555fc6f3218f
zerocho <zerohch0@gmail.com>
2023-11-23 16:29:50 +0900

    Fix: /compose/tweet에서 새로고침 시

--- lecture/src/app/(afterLogin)/compose/tweet/page.tsx
+++ lecture/src/app/(afterLogin)/compose/tweet/page.tsx
@@ -1,7 +1,11 @@
 import Home from "@/app/(afterLogin)/home/page";
+import TweetModal from "@/app/(afterLogin)/@modal/(.)compose/tweet/page";
 
 export default function Page() {
   return (
-    <Home />
+    <>
+      <Home/>
+      <TweetModal/>
+    </>
   )
 }
