a1097c05efca2b6f9c6a38b8157c55856267edbb
zerocho <zerohch0@gmail.com>
2024-05-27 17:53:02 +0900

    fix: 로그아웃해도 이전 세션 캐싱되어 있는 문제

--- lecture/src/app/(afterLogin)/_component/LogoutButton.tsx
+++ lecture/src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -2,7 +2,6 @@
 
 import style from "./logoutButton.module.css";
 import {signOut} from "next-auth/react";
-import {useRouter} from "next/navigation";
 import {Session} from "@auth/core/types";
 import {useQueryClient} from "@tanstack/react-query";
 
@@ -10,7 +9,6 @@ type Props = {
   me: Session | null
 }
 export default function LogoutButton({ me }: Props) {
-  const router = useRouter();
   const queryClient = useQueryClient();
 
   const onLogout = () => {
@@ -20,14 +18,7 @@ export default function LogoutButton({ me }: Props) {
     queryClient.invalidateQueries({
       queryKey: ["users"],
     });
-    signOut({ redirect: false })
-      .then(() => {
-        fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/logout`, {
-          method: 'post',
-          credentials: 'include',
-        });
-        router.replace('/');
-      });
+    signOut({ callbackUrl: '/' });
   };
 
   if (!me?.user) {
